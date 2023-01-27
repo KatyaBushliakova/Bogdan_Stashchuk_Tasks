2a435cfed652f3fd90831315002c83011177018d
Ekaterina <katya_baumit@mail.ru>
2023-01-27 21:20:30 +0300

    6

---
+++ TASKS/06-data-types/finish.js
@@ -0,0 +1,29 @@
+/** ЗАДАЧА 6 - Типы данных
+ *
+ * 1. Объявите несколько переменных и присвойте им значения:
+ *  - строка
+ *  - число
+ *  - логическое
+ *  - null
+ *  - undefined
+ *  - объект
+ *  - массив
+ *
+ * 2. Выведите в консоль тип каждого из значений
+ * используя оператор typeof
+ */
+const myString = "Katya"
+const myNumber = 12
+const myBoolean = true
+const myNull = null
+let myUndefiend
+const myObject = {}
+const myArray = []
+
+console.log(typeof myString)
+console.log(typeof myNumber)
+console.log(typeof myBoolean)
+console.log(typeof myNull)
+console.log(typeof myUndefiend)
+console.log(typeof myObject)
+console.log(typeof myArray)
