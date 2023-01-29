84fc828b6f06fcceda7780582949053b8a3e95e6
Ekaterina <katya_baumit@mail.ru>
2023-01-29 18:17:52 +0300

    12

---
+++ TASKS/12-create-object/finish.js
@@ -0,0 +1,19 @@
+/** ЗАДАЧА 12 - Создание объекта
+ *
+ * 1. Создайте объект с тремя свойствами:
+ *  - name
+ *  - surname
+ *  - favoriteNumber
+ *
+ * 2. Выведите в консоль строку
+ * "My name is <name> <surname> and my favorite number is <favoriteNumber>"
+ */
+
+const person = {
+    name: "Ekaterina",
+    surname: "Bushliakova",
+    favNumber: 12,
+}
+
+const personInfo = `My name is ${person.name} ${person.surname} and my favorite number is ${person.favNumber}`
+console.log(personInfo)
