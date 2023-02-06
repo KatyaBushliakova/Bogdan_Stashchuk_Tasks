5d8a9284230926a8b8c052769145a3b8b8aa9bcf
Ekaterina <katya_baumit@mail.ru>
2023-02-06 19:12:18 +0300

    19

---
+++ TASKS/19-push-to-array/finish.js
@@ -0,0 +1,19 @@
+/** ЗАДАЧА 19 - Добавление элементов в массив
+ *
+ * 1. Создайте любой массив
+ *
+ * 2. Добавьте в конец массива несколько новых элементов
+ *
+ * 3. Выведите в консоль длину результирующего массива
+ */
+
+const myNumbers = [2, 5, 12, 19]
+
+myNumbers.push(10)
+
+console.log(myNumbers)
+
+myNumbers.push(6, 19)
+
+console.log(myNumbers)
+console.log(myNumbers.length)
